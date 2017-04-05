d8d281121bb30637f3f91755098aaee9396f581b
svranic <svranic@geof.hr>
2017-04-05 12:57:39 +0200

    Update apache2_php7_install.sh

--- apache2_php7_install.sh
+++ apache2_php7_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+debianVersion='jessie'
+
 # Check to see if there are any updates of OS
 apt-get update
 
@@ -10,24 +12,17 @@ apt-get update
 # /etc/apache2/sites-enabled (virtual hosts) and /etc/apache2/conf-enabled
 apt-get -y install apache2
 
-# Install PHP5 and Apache PHP5 module
-#apt-get -y install php5 libapache2-mod-php5
-
-# Remove PHP 5 (in order to install PHP 7)
-#x="(dpkg --list | grep php | awk '/^ii/{ print $2}')"
-#apt-get --purge remove $x
-
 #Additional repository for Debian (Containts PHP 7.0.13. for Debian Jessie)
 # Add repository
 echo '' >> /etc/apt/sources.list
-echo '#PHP7 repository' >> /etc/apt/sources.list
-echo 'deb http://packages.dotdeb.org wheezy all' >> /etc/apt/sources.list
-echo 'deb-src http://packages.dotdeb.org wheezy all' >> /etc/apt/sources.list
+echo "#PHP7 repository" >> /etc/apt/sources.list
+echo "deb http://packages.dotdeb.org $debianVersion all" >> /etc/apt/sources.list
+echo "deb-src http://packages.dotdeb.org $debianVersion all" >> /etc/apt/sources.list
 
 cd /tmp
 wget https://www.dotdeb.org/dotdeb.gpg
 apt-key add dotdeb.gpg
-rm dotdeb.gpg
+#rm dotdeb.gpg
 
 apt-get update
 apt-get install php7.0
@@ -38,6 +33,7 @@ apt-get -y install php-pgsql
 apt-get -y install php-sqlite3
 
 # Check versions
+# If running apachectl from shell needs to be root or use sudo
 apachectl -V
 php -v
 
